3f24d0927c86894b22f248217fd89da39b9c2405
Maggie29 <zmagali@yahoo.com>
2017-03-29 13:36:28 -0500

    first Commit

---
+++ assets/js/registro.js
@@ -0,0 +1,42 @@
+window.onload = function(){
+document.getElementById("form").onsubmit = function(e){
+  e.preventDefault();
+}
+function registroCoder(name, lastName, email, password){
+  this.name = name;
+  this.lastName = lastName;
+  this.email = email;
+  this.password = password;
+};
+
+var coder = [];
+document.getElementById("signUp").addEventListener("click", function(){
+  var name = document.getElementById("name").value;
+  var lastName = document.getElementById("lastName").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value
+  if(name.length!=0 && lastName.length!=0 && email.length!=0 && password!=0){
+  coder.push(new registroCoder(name, lastName,email, password));
+  creandoDiv(coder);
+  document.getElementById("form").reset()
+}else{
+    var error = document.getElementById("alertError");
+    error.innerText = "Todo los campos son obligatorios";
+  }
+});
+
+function creandoDiv(){
+  var contenedor = document.getElementById("contenedor");
+  contenedor.innerHTML = "";
+  for(var i = 0; i<coder.length;i++){
+  			var divContenedor= document.createElement("div");
+        divContenedor.classList.add("div-coder");
+  			for (var prop in coder[i]) {
+  				var pContenedor= document.createElement("p");
+  				pContenedor.innerHTML= prop + " : " + coder[i][prop];
+  				divContenedor.appendChild(pContenedor);
+        }
+        contenedor.appendChild(divContenedor);
+      }
+    }
+}
